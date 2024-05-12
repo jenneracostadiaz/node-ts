83fe35bdfc359acd531c4b46a04c4a85d904b056
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2024-05-11 20:04:31 -0500

    Init

---
+++ curso-node-ts/src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+class ServerBoostrap {
+	public app: express.Application = express();
+	private port: number = 8000;
+
+	constructor() {
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+		this.app.use(morgan('dev'));
+		this.app.use(cors());
+
+		this.app.get('/api', (req, res) => {
+			res.status(200).json({ message: 'Hello World!' });
+		});
+		this.listen();
+	}
+
+	public listen() {
+		this.app.listen(this.port, () => {
+			console.log(`Server running on port ${this.port}`);
+		});
+	}
+}
+
+new ServerBoostrap();
